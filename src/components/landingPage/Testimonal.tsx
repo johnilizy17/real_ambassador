ace7e3339ec7d0ac538ef400aace77fa748c3911
John Abraham <johnabrahamtosin@gmail.com>
2025-06-03 09:08:03 +0100

    arrow direction

--- src/components/landingPage/Testimonal.tsx
+++ src/components/landingPage/Testimonal.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 declare global {
     interface Window {
         $: any;
@@ -7,26 +7,78 @@ declare global {
 }
 
 export default function Testimonial() {
+
+    const [pageSize, setPageSize] = useState({
+        width: 0,
+        height: 0
+    });
+
     useEffect(() => {
-        const initCarousel = () => {
-            if (window?.$ && typeof window.$(".owl-carousel").owlCarousel === "function") {
-                window.$(".owl-carousel").owlCarousel({
-                    loop: true,
-                    margin: 10,
-                    nav: true,
-                    dots: true,
-                    items: 1,
-                    autoplay: true,
-                    autoplayTimeout: 3000,
-                    autoplayHoverPause: true,
-                });
-            } else {
-                setTimeout(initCarousel, 300);
-            }
+        const updateSize = () => {
+            setPageSize({
+                width: window.innerWidth,
+                height: window.innerHeight
+            });
         };
 
-        initCarousel();
-    }, []);
+        // Initial size
+        updateSize();
+
+        // Listen for resize events
+        window.addEventListener('resize', updateSize);
+
+        if (pageSize.width != 0) {
+            if (pageSize.width < 700) {
+                const initCarousel = () => {
+                    if (window?.$ && typeof window.$(".owl-carousel").owlCarousel === "function") {
+                        window.$(".owl-carousel").owlCarousel({
+                            loop: true,
+                            margin: 10,
+                            nav: true,
+                            dots: true,
+                            items: 1,
+                            autoplay: true,
+                            autoplayTimeout: 3000,
+                            autoplayHoverPause: true,
+                            navText: [
+                                "<i class='fa fa-chevron-left'></i>",
+                                "<i class='fa fa-chevron-right'></i>"
+                            ]
+
+                        });
+                    } else {
+                        setTimeout(initCarousel, 300);
+                    }
+                }
+                initCarousel();
+            } else {
+                const initCarousel = () => {
+                    if (window?.$ && typeof window.$(".owl-carousel").owlCarousel === "function") {
+                        window.$(".owl-carousel").owlCarousel({
+                            loop: true,
+                            margin: 10,
+                            nav: true,
+                            dots: true,
+                            items: 3,
+                            autoplay: true,
+                            autoplayTimeout: 3000,
+                            autoplayHoverPause: true,
+                            navText: [
+                                "<i class='fa fa-chevron-left'></i>",
+                                "<i class='fa fa-chevron-right'></i>"
+                            ]
+
+                        });
+                    } else {
+                        setTimeout(initCarousel, 300);
+                    }
+                }
+                initCarousel();
+            }
+        }
+
+
+    }, [pageSize.width]);
 
     return (
         <div className="testimonial_wrapper slider_btn_min float_left">
@@ -52,10 +104,10 @@ export default function Testimonial() {
                                             <img src="images/cnt1.png" alt="Ambassador 1" />
                                         </div>
                                         <div className="saying_img_name">
-                                            <h1><a href="#" style={{color:"#fff"}}>Amara Okoye</a></h1>
+                                            <h1><a href="#" style={{ color: "#fff" }}>Amara Okoye</a></h1>
                                             <p>Lead Ambassador</p>
                                         </div>
-                                        <p style={{color:"#fff"}}>
+                                        <p style={{ color: "#fff" }}>
                                             “Joining the ABN Nariohs Ambassador Program has empowered me to connect with amazing people and make a real difference.”
                                         </p>
                                     </div>
@@ -66,10 +118,10 @@ export default function Testimonial() {
                                             <img src="images/cnt2.png" alt="Ambassador 2" />
                                         </div>
                                         <div className="saying_img_name">
-                                            <h1><a href="#" style={{color:"#fff"}}>Chinedu Eze</a></h1>
+                                            <h1><a href="#" style={{ color: "#fff" }}>Chinedu Eze</a></h1>
                                             <p>Community Ambassador</p>
                                         </div>
-                                        <p style={{color:"#fff"}}>
+                                        <p style={{ color: "#fff" }}>
                                             “Being part of this program gave me the platform to share my passion and grow my network globally.”
                                         </p>
                                     </div>
@@ -80,10 +132,10 @@ export default function Testimonial() {
                                             <img src="images/cnt.png" alt="Ambassador 3" />
                                         </div>
                                         <div className="saying_img_name">
-                                            <h1><a href="#" style={{color:"#fff"}}>Nkechi Nwosu</a></h1>
+                                            <h1><a href="#" style={{ color: "#fff" }}>Nkechi Nwosu</a></h1>
                                             <p>Social Media Ambassador</p>
                                         </div>
-                                        <p style={{color:"#fff"}}>
+                                        <p style={{ color: "#fff" }}>
                                             “The ABN Nariohs Ambassador Program has been a rewarding journey of learning, leadership, and impact.”
                                         </p>
                                     </div>
