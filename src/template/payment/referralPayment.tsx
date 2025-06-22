daea2907869f76212e802c3807c5b6f8eebba668
John Abraham <johnabrahamtosin@gmail.com>
2025-06-22 15:16:54 +0100

    plan change

--- src/template/payment/referralPayment.tsx
+++ src/template/payment/referralPayment.tsx
@@ -2,7 +2,7 @@ import useCustomToast from "@/hooks/useCustomToast";
 import { userActive } from "@/url/api's/userProfile";
 import { closePaymentModal, useFlutterwave } from "flutterwave-react-v3";
 import { useRouter } from "next/router";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import AccountGeneration from "./Account";
 
@@ -22,6 +22,7 @@ export default function ReferralPaymentFlutterwave({
 
     const router = useRouter()
     const showToast = useCustomToast();
+    const [select, setSelect] = useState(false)
 
     async function PaymentActivation() {
         showToast("Subscription successful", "success")
