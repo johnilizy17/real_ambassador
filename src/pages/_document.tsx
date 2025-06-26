f39d6335de1cf6c3db2fd8473727eebeaee3bdf5
John Abraham <johnabrahamtosin@gmail.com>
2025-06-26 13:04:00 +0100

    new changes

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,14 @@
-import { Html, Head, Main, NextScript } from "next/document";
+// pages/_document.js
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html>
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="/logo/logo_b.png" />
+        <meta name="theme-color" content="#000000" />
+      </Head>
       <body>
         <Main />
         <NextScript />
