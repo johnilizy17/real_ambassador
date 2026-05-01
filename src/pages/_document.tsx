dbb777e9df3558dfe48b1688cfe4ad1390788af9
johnilizy17 <abrahamtosinj@gmail.com>
2026-05-01 06:14:15 +0100

    full admin

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,22 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        {/* Google Analytics */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-WPV1ZZ9K49"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-WPV1ZZ9K49');
+            `,
+          }}
+        />
+        
         <link rel="manifest" href="/manifest.json" />
         <link rel="icon" href="/logo/logo_b.png" />
         <meta name="theme-color" content="#000000" />
